dc2da466b40cb1a65dbc49cad5d1dd07a4d025ea
haidarrzn07@gmail.com <haidarrzn06@gmail.com>
2025-03-13 10:37:59 +0700

    upload pratikum04

--- praktikum-3a/partials/sections/copyright.php
+++
@@ -1,3 +0,0 @@
-<div class="copyright py-4 text-center text-white">
-            <div class="container"><small>Copyright &copy; Your Website 2023</small></div>
-        </div>
--- praktikum-3b/partials/sections/education-section.php
+++
@@ -1,8 +0,0 @@
-<!-- Education Section-->
-<section>
-            <h2 class="text-secondary fw-bolder mb-4">Education</h2>
-            <!-- education Card -->
-            <?php 
-            include_once "partials/cards/education-card-1.php";
-            include_once "partials/cards/education-card-2.php";?>
-</section>
