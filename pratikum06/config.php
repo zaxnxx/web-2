c93c853c0f60da18e2c1901617ec09ed7a4229cf
haidarrzn07@gmail.com <haidarrzn06@gmail.com>
2025-05-08 10:12:39 +0700

    pratikum07

--- pratikum06/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$host = 'localhost';
-$db = 'learn_pdo';
-$user = 'root';
-$password = '';
-?>
--- pratikum06/test-connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = require 'Connection.php';
-$statement = $pdo->query('select * from users');
-print_r($statement->fetchAll())
-?>
